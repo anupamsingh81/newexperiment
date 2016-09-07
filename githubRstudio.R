14a22c53a41c558d783afe0de858a9733e00cc11
anupamsingh81 <anupampom@gmail.com>
2016-09-08 04:37:49 +0530

    added two new files

---
+++ githubRstudio.R
@@ -0,0 +1,11 @@
+# local git rpository at time of project
+
+# execute commands in shell
+
+# creating repository in github
+
+# git remote add origin https://github.com/anupamsingh81/miscellcode.git
+# configure github repository
+# git config remote.origin.url git@github.com:anupamsingh81/miscellcode.git
+# git pull -u origin master
+# git push -u origin master
